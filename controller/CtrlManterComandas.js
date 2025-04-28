8ac661e2ec1601ab04125e08cc7bd41bfd58237f
Rafael Ruddy <rafaelruddy@gmail.com>
2025-04-28 10:35:49 -0300

    fix ctrlmantermesas

---
+++ controller/CtrlManterComandas.js
@@ -0,0 +1,128 @@
+"use strict";
+
+import Status from "/model/Status.js";
+import Comanda from "/model/Comanda.js";
+import DaoComanda from "/model/dao/daoComanda.js";
+import ViewerGarcom from "/viewer/ViewerGarcom.js";
+
+export default class CtrlManterGarcons {
+
+  #daoComanda;
+  #viewer;
+  #posAtual;
+  #status;
+
+  constructor() {
+    this.#daoComanda = new DaoComanda();
+    this.#viewer = new ViewerGarcom(this);
+    this.#posAtual = 1;
+    this.#atualizarContextoNavegacao();
+  }
+
+  async #atualizarContextoNavegacao() {
+    this.#status = Status.NAVEGANDO;
+    this.#viewer.statusApresentacao();
+    let conjGarcons = await this.#daoComanda.obterGarcons();
+    if (conjGarcons.length === 0) {
+      this.#posAtual = 0;
+      this.#viewer.apresentar(0, 0, null);
+    } else {
+      if (this.#posAtual === 0 || this.#posAtual > conjGarcons.length)
+        this.#posAtual = 1;
+      this.#viewer.apresentar(this.#posAtual, conjGarcons.length, new GarcomDTO(conjGarcons[this.#posAtual - 1]));
+    }
+  }
+
+  async apresentarPrimeiro() {
+    let conjGarcons = await this.#daoComanda.obterGarcons();
+    if (conjGarcons.length > 0) this.#posAtual = 1;
+    this.#atualizarContextoNavegacao();
+  }
+
+  async apresentarProximo() {
+    let conjGarcons = await this.#daoComanda.obterGarcons();
+    if (this.#posAtual < conjGarcons.length) this.#posAtual++;
+    this.#atualizarContextoNavegacao();
+  }
+
+  async apresentarAnterior() {
+    let conjGarcons = await this.#daoComanda.obterGarcons();
+    if (this.#posAtual > 1) this.#posAtual--;
+    this.#atualizarContextoNavegacao();
+  }
+
+  async apresentarUltimo() {
+    let conjGarcons = await this.#daoComanda.obterGarcons();
+    this.#posAtual = conjGarcons.length;
+    this.#atualizarContextoNavegacao();
+  }
+
+  iniciarIncluir() {
+    this.#status = Status.INCLUINDO;
+    this.#viewer.statusEdicao(Status.INCLUINDO);
+    this.efetivar = this.incluir;
+  }
+
+  iniciarAlterar() {
+    this.#status = Status.ALTERANDO;
+    this.#viewer.statusEdicao(Status.ALTERANDO);
+    this.efetivar = this.alterar;
+  }
+
+  iniciarExcluir() {
+    this.#status = Status.EXCLUINDO;
+    this.#viewer.statusEdicao(Status.EXCLUINDO);
+    this.efetivar = this.excluir;
+  }
+
+  async incluir(matricula, nome, email, horaInicio, horaFim, situacao) {
+    if (this.#status === Status.INCLUINDO) {
+      try {
+        let garcom = new Garcom(nome, email, parseInt(matricula), horaInicio, horaFim, situacao);
+        await this.#daoComanda.incluir(garcom);
+        this.#status = Status.NAVEGANDO;
+        this.#atualizarContextoNavegacao();
+      } catch (e) {
+        alert(e);
+      }
+    }
+  }
+
+  async alterar(matricula, nome, email, horaInicio, horaFim, situacao) {
+    if (this.#status === Status.ALTERANDO) {
+      try {
+        let garcom = new Garcom(nome, email, parseInt(matricula), horaInicio, horaFim, situacao);
+        await this.#daoComanda.alterar(garcom);
+        this.#status = Status.NAVEGANDO;
+        this.#atualizarContextoNavegacao();
+      } catch (e) {
+        alert(e);
+      }
+    }
+  }
+
+  async excluir(matricula) {
+    if (this.#status === Status.EXCLUINDO) {
+      try {
+        let garcom = new Garcom("", "", parseInt(matricula), "00:00", "00:00", "INATIVO");
+        await this.#daoComanda.excluir(garcom);
+        this.#status = Status.NAVEGANDO;
+        this.#atualizarContextoNavegacao();
+      } catch (e) {
+        alert(e);
+      }
+    }
+  }
+
+  cancelar() {
+    this.#atualizarContextoNavegacao();
+  }
+
+  sair() {
+    window.location.href = "/menu.html"; // <- aqui ajusta o caminho para o seu Menu
+  }
+
+  getStatus() {
+    return this.#status;
+  }
+}
