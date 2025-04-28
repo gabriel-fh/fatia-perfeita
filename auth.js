d67f1056d687e01a39e8bb3e826f29d31181161d
gabriel-fh <gabriel.f.hermenegildo>
2025-04-27 22:22:15 -0300

    lixo

--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js"; // Importe get também
 
 const firebaseConfig = {
   apiKey: "AIzaSyD83f2LeSDIuVWBFUnu_jHOzshsjDkF5iI",
@@ -16,13 +13,29 @@ const firebaseConfig = {
   measurementId: "G-57F0MG0EYV",
 };
 
-// Initialize Firebase
+// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getDatabase(app); // Obtém a referência ao banco de dados
+
+const uid = "lO5TUYCUvyZap0C10XDwEda5mx22"; // UID do usuário, pode ser o auth.uid se for o usuário autenticado
+const userRef = ref(db, `usuarios/${uid}`); // Acessa a referência do usuário diretamente
+
+get(userRef)
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log("Usuário encontrado:", snapshot.val());
+    } else {
+      console.log("Usuário não encontrado com o UID:", uid);
+    }
+  })
+  .catch((error) => {
+    console.error("Erro ao buscar dados:", error);
+  });
+
 
 // Função para fazer login
 const signInUser = (email, password) => {
-
   const form = document.getElementById("login-form");
   const hiddenElements = document.querySelectorAll(".hidden");
   const main = document.getElementsByTagName("main")[0];
@@ -32,9 +45,9 @@ const signInUser = (email, password) => {
       console.log("Usuário logado: ", user);
       hiddenElements.forEach((element) => {
         element.classList.remove("hidden");
-      })
-      form.classList.add("hidden")
-      main.classList.remove("flex-center")
+      });
+      form.classList.add("hidden");
+      main.classList.remove("flex-center");
       localStorage.setItem("user", JSON.stringify(user));
     })
     .catch((error) => {
@@ -47,7 +60,7 @@ const signInUser = (email, password) => {
 };
 
 document.getElementById("login-form").addEventListener("submit", function (event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
